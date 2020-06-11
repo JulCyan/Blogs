37b1d18178e29099f2bf69d430c1f84f494c46d9
chenshuai <shuaichenh@isoftstone.com>
2020-06-11 17:35:08 +0800

    Config sh:automatically push to github:gh-pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    title: 'Cyan\' Blogs',
-    description: 'Just playing around'
+  base: '/Blogs/',
+  title: 'JulCyan\' Blogs',
+  description: 'Full Stack Developer\' Blogs',
+  port: 8099,
+  // markdown
+  markdown: {
+    lineNumbers: true
+  },
+  evergreen: true
 }
